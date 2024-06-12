11e523d92c1ce237fe94896f5e835a6fe5263d76
dangoodie <dan.gooden.dev@gmail.com>
2024-06-12 23:53:28 +1000

    BackSpace working as intended

--- internal/editor/input.go
+++ internal/editor/input.go
@@ -2,25 +2,28 @@ package editor
 
 import (
 	"fmt"
+    structs "github.com/dangoodie/sqtxt/internal/structs"
 )
 
 func (e *Editor) HandleKeyInput(key string) {
 	switch key {
 	case "Escape":
-		fmt.Println("Escape key pressed")
+		fmt.Println("Quitting...")
 		e.Quit()
 	case "Left": // Left arrow
-		fmt.Println("Left arrow")
 		e.Cursor.Col--
 	case "Up": // Up arrow
-		fmt.Println("Up arrow")
 		e.Cursor.Row--
 	case "Right": // Right arrow
-		fmt.Println("Right arrow")
 		e.Cursor.Col++
 	case "Down": // Down arrow
-		fmt.Println("Down arrow")
         e.Cursor.Row++
+    case "BackSpace":
+        delPos := structs.Position{e.Cursor.Row, e.Cursor.Col - 1}
+        e.Buffer.Delete(delPos)
+        e.Cursor.Col--
+    default:
+        fmt.Println("Unknown key:", key)
 	}
 }
 
